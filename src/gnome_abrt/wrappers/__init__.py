1204b373b232ac4ea6460779df38493e46a40acc
Bastien Nocera <hadess@hadess.net>
2015-02-13 13:49:59 +0100

    Remove "ABRT Configuration" dialogue

--- src/gnome_abrt/wrappers/__init__.py
+++ src/gnome_abrt/wrappers/__init__.py
@@ -17,6 +17,5 @@
 
 #pylint: disable=E0611
 from gnome_abrt.wrappers._wrappers import (show_events_list_dialog,
-                                           show_system_config_abrt_dialog,
                                            show_problem_details_for_dir,
                                            get_app_for_cmdline)
